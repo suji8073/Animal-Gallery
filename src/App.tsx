eaee0f8c00813ec92c6c24ad4ee4c4e26da4a740
Suji Chae <63655826+suji8073@users.noreply.github.com>
2023-07-06 18:02:14 +0900

    feat: Image API 연동

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [catImages, setCatImages] = useState([]);
+
+  useEffect(() => {
+    fetchCatImages();
+  }, []);
+
+  const fetchCatImages = async () => {
+    const response = await fetch(
+      `https://api.unsplash.com/photos/random?count=15&query=cat&client_id=${process.env.REACT_APP_UNSPLASH_TOKEN}`
+    );
+    const data = await response.json();
+    const imageUrls = data.map(
+      (image: { [key: string]: any }) => image.urls.regular
+    );
+
+    setCatImages(imageUrls);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {catImages.map((imageUrl) => (
+        <img className="Image" src={imageUrl} alt="Cat" />
+      ))}
     </div>
   );
 }
